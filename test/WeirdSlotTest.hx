44eaae96d4d832e9d670f513735a63f132ae8430
Heorhiy Kharvat <zaynyatyi@gmail.com>
2016-08-27 15:08:44 +0300

    empty tests added
    used munit for tests

---
+++ test/WeirdSlotTest.hx
@@ -0,0 +1,32 @@
+package;
+
+import massive.munit.util.Timer;
+import massive.munit.Assert;
+import massive.munit.async.AsyncFactory;
+
+class WeirdSlotTest
+{
+	public function new()
+	{
+	}
+
+	@BeforeClass
+	public function beforeClass():Void
+	{
+	}
+
+	@AfterClass
+	public function afterClass():Void
+	{
+	}
+
+	@Before
+	public function setup():Void
+	{
+	}
+
+	@After
+	public function tearDown():Void
+	{
+	}
+}
