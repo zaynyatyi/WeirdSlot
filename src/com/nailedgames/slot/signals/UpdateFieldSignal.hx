5af26340f4146712b707a73f58606dad17568e91
Heorhiy Kharvat <zaynyatyi@gmail.com>
2016-08-27 23:10:40 +0300

    Initial game signals added

---
+++ src/com/nailedgames/slot/signals/UpdateFieldSignal.hx
@@ -0,0 +1,15 @@
+package com.nailedgames.slot.signals;
+
+import msignal.Signal.Signal0;
+
+/**
+ * ...
+ * @author Heorhiy Kharvat
+ */
+class UpdateFieldSignal extends Signal0
+{
+	public function new()
+	{
+		super();
+	}
+}
