6f3cb6b504389c6ba2fb0e78b91fd0e43258ea01
Heorhiy Kharvat <zaynyatyi@gmail.com>
2016-08-27 23:15:01 +0300

    Commands, signals, models and views wired in application context

--- src/com/nailedgames/app/ApplicationContext.hx
+++ src/com/nailedgames/app/ApplicationContext.hx
@@ -2,7 +2,15 @@ package com.nailedgames.app;
 
 import com.nailedgames.app.ApplicationView;
 import com.nailedgames.app.ApplicationViewMediator;
+import com.nailedgames.slot.commands.InitializeCommand;
+import com.nailedgames.slot.commands.LoopCommand;
+import com.nailedgames.slot.models.CardModel;
 import com.nailedgames.slot.models.FieldModel;
+import com.nailedgames.slot.models.RollModel;
+import com.nailedgames.slot.models.RollsModel;
+import com.nailedgames.slot.signals.InitializeSignal;
+import com.nailedgames.slot.signals.LoopSignal;
+import com.nailedgames.slot.signals.UpdateFieldSignal;
 import com.nailedgames.slot.views.FieldView;
 import mmvc.api.IViewContainer;
 import mmvc.impl.Context;
@@ -17,11 +25,22 @@ class ApplicationContext extends Context
 
 	override public function startup():Void
 	{
+		super.startup();
+		//Mapping commands and signals
+		commandMap.mapSignalClass(InitializeSignal, InitializeCommand, true);
+		commandMap.mapSignalClass(LoopSignal, LoopCommand);
+
 		//Mapping models
+		injector.mapSingleton(CardModel);
+		injector.mapSingleton(RollModel);
+		injector.mapSingleton(RollsModel);
 		injector.mapSingleton(FieldModel);
 
 		//Mapping views and mediators
 		mediatorMap.mapView(FieldView, FieldViewMediator);
+
+		//Mapping signals which will be used by mediators to update related views
+		injector.mapSingleton(UpdateFieldSignal);
 
 		//Wiring for main application module
 		mediatorMap.mapView(ApplicationView, ApplicationViewMediator);
@@ -33,5 +52,6 @@ class ApplicationContext extends Context
 	*/
 	override public function shutdown()
 	{
+		super.shutdown();
 	}
 }
