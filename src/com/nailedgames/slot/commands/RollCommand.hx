c47d67240d45c45196c52645625f1f4b9ce9bae0
Heorhiy Kharvat <zaynyatyi@gmail.com>
2016-08-28 16:02:23 +0300

    Roll will be finished when rows are straight on their positions

--- src/com/nailedgames/slot/commands/RollCommand.hx
+++ src/com/nailedgames/slot/commands/RollCommand.hx
@@ -1,6 +1,8 @@
 package com.nailedgames.slot.commands;
 
+import com.nailedgames.slot.models.CardModel;
 import com.nailedgames.slot.models.FieldModel;
+import com.nailedgames.slot.models.RollModel;
 import com.nailedgames.slot.signals.UpdateFieldSignal;
 import com.nailedgames.utils.Settings;
 import mmvc.impl.Command;
@@ -22,19 +24,46 @@ class RollCommand extends Command
 		var modified:Bool = false;
 		for (rollModel in fieldModel.rollsModel.collection) {
 			if (rollModel.speed > 0) {
+				rollModel.shift += rollModel.speed;
 				for (cardModel in rollModel.cards) {
 					if (cardModel.y > settings.fieldRows * settings.squareHeight) {
 						cardModel.y -= settings.chunksCollection.collection.length * settings.squareHeight;
 					}
 					cardModel.y += rollModel.speed;
 				}
-				rollModel.iterations--;
-				if (rollModel.iterations == 0) {
-					rollModel.speed = 0;
+				if (rollModel.isFinishing != true) {
+					rollModel.iterations--;
+					if (rollModel.iterations == 0) {
+						if (rollModel.shift % settings.squareHeight == 0) {
+							resetMovement(rollModel);
+						} else {
+							rollModel.isFinishing = true;
+							rollModel.speed = 1;
+						}
+					}
+				} else {
+					var delta:Float = rollModel.shift % settings.squareHeight;
+					if (delta < 1) {
+						resetMovement(rollModel);
+						fixPositions(rollModel.cards, delta);
+					}
 				}
 				modified = true;
 			}
 		}
 		if (modified) updateFieldSignal.dispatch();
+	}
+
+	function fixPositions(cards:Array<CardModel>, delta:Float) {
+		for (cardModel in cards) {
+			cardModel.y = Math.round(cardModel.y - delta);
+		}
+	}
+
+	function resetMovement(rollModel:RollModel):Void
+	{
+		rollModel.speed = 0;
+		rollModel.shift = 0;
+		rollModel.isFinishing = false;
 	}
 }
--- src/com/nailedgames/slot/models/RollModel.hx
+++ src/com/nailedgames/slot/models/RollModel.hx
@@ -10,7 +10,9 @@ class RollModel extends Actor
 {
 	public var cards:Array<CardModel>;
 	public var speed:Float = 0;
+	public var shift:Float = 0;
 	public var iterations:Int = 0;
+	public var isFinishing:Bool = false;
 
 	public function new()
 	{
